a041ed7738b6f4fa27ca72abd775f23fa30f09b7
Justin Suen <jsuen27@gmail.com>
2017-01-27 21:02:33 -0800

    Set up node-sass to compile css

---
+++ frontend/reducers/project_reducer.js
@@ -0,0 +1,12 @@
+import merge from 'lodash/merge';
+
+const ProjectReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default ProjectReducer;
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import ProjectReducer from './project_reducer';
 
 const rootReducer = combineReducers({
+  project: ProjectReducer
 });
 
 export default rootReducer;
