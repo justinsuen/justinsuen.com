32d4c4fddd7420f4b3fcc2ca3b9486f067fbbf69
Justin Suen <jsuen27@gmail.com>
2017-02-09 21:56:07 -0800

    Basic Flask app working

--- blog.py
+++ blog.py
@@ -40,4 +40,4 @@ def construct_blog_posts(path):
     return reversed(sorted(blog_posts))
 
 if __name__ == '__main__':
-    print [(post['title'], post['id']) for post in construct_blog_posts('static/assets/posts/')]
+    print([(post['title'], post['id']) for post in construct_blog_posts('static/assets/posts/')])
