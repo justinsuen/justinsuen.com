ae64f69686df5be784e65e8cf3358cc54d8560d2
Justin Suen <jsuen27@gmail.com>
2017-02-11 21:21:31 -0800

    Add some jQuery to scrolling navbar links

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(document).on("scroll", function() {
+    let windscroll = $(document).scrollTop();
+    $('body section').each((i, obj) => {
+      if ($(obj).position().top <= windscroll + 300) {
+        $('.navbar-links a.active').removeClass('active');
+        $('.navbar-links a[data-scroll=' + obj.id + ']').addClass('active');
+      }
+    });
+  });
+});
